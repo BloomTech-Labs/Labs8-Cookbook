bbb1ad890ecdca1f499e03a7aebcab781aa9c8a5
Katie Gorbell <katiegorbell@gmail.com>
2018-11-07 17:36:04 -0600

    design files added to branch, basidc laout for landing page established

--- cookbook/src/Components/LandingPage.js
+++ cookbook/src/Components/LandingPage.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import titleImg from '../Images/titleImg.jpeg'
+import { Link } from 'react-router-dom';
+
+import titleImg from '../Images/titleImg.jpeg';
+import logo from '../designs/Logo/CookBookLogo.svg';
+
 
 const LandingPage = () => {
     return ( 
         <div className='containerLP'>
             <div className='titleContainer'>
-                <img className="titleImg" src={titleImg} alt="Image of a table with produce on top." />
-                <img className="logo" />
-                <span className='title'>COOKBOOK</span>
+                <img className="bckgImg" src={titleImg} alt="Image of a table with produce on top." />
+                <div className='landingNav'>
+                    <Link className='login' to='signin'>Login</Link>
+                    <Link className='register' to='signup'>Register</Link>
+                </div>
+                    <img className="logo" src={logo} />
+                    <span className='title'>COOKBOOK</span>
             </div>
         </div>
-        
+
      );
 }
  
