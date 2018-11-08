82dde52a7bcfc5813f355893bfc72301347a462e
Katie Gorbell <katiegorbell@gmail.com>
2018-11-08 13:06:59 -0600

    error adding nav icon to header

--- cookbook/src/Components/Header.js
+++ cookbook/src/Components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Logo from '../designs/Logo/CookBookLogo.svg';
+import NavIcon from './NavIcon';
 
 
 const Header = () => {
     return ( 
         <div className='header'>
-
+            <img className='logo' src={Logo} alt='COOKBOOK logo'/>
         </div> 
     );
 }
---
+++ cookbook/src/Components/NavIcon.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'React';
+
+
+class NavIcon extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default NavIcon;
