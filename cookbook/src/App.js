835de2d5f949e27ef92f3a598ea2e4c17e3f2314
Vu Cao <cdhvu@yahoo.com>
2018-11-13 12:08:07 -0800

    Create Home component and small changes to css
    Remove unused styled-components
    Add box-sizing border-box in index.less
    Remove display flex for .app
    Remove margin for header, and set top 0
    Add home.less

--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react';
-import './Styles/css/index.css';
+import React, { Component } from "react";
+import "./Styles/css/index.css";
 
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 
-import LandingPage from './Components/LandingPage';
-import Signin from './Components/Signin';
-import Signup from './Components/Signup';
-import Footer from './Components/Footer';
-
+import Home from "./Components/Home";
+import LandingPage from "./Components/LandingPage";
+import Signin from "./Components/Signin";
+import Signup from "./Components/Signup";
+import Footer from "./Components/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Route exact path='/' component={LandingPage}/>
-        <Route path='/signin' render={ (props) =>
-          { return(<Signin {...props} />) } }
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/home" component={Home} />
+        <Route
+          path="/signin"
+          render={props => {
+            return <Signin {...props} />;
+          }}
         />
-        <Route path='/signup' render={ (props) =>
-          { return(<Signup {...props} />) } }
+        <Route
+          path="/signup"
+          render={props => {
+            return <Signup {...props} />;
+          }}
         />
         <Footer />
       </div>
