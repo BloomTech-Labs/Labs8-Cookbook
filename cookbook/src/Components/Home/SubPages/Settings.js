1d23560c1a3a682beb20418cb7021e412cb3e5af
Katie Gorbell <katiegorbell@gmail.com>
2018-11-14 16:53:26 -0600

    Stripe form added to FE

--- cookbook/src/Components/Home/SubPages/Settings.js
+++ cookbook/src/Components/Home/SubPages/Settings.js
@@ -1,7 +1,34 @@
-import React from "react";
 
-const Settings = () => {
-  return <div>This is Settings page</div>;
-};
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout';
 
-export default Settings;
+export default class Settings extends React.Component {
+  onToken = (token) => {
+    fetch('/save-stripe-token', {
+      method: 'POST',
+      body: JSON.stringify(token),
+    }).then(response => {
+      response.json().then(data => {
+        alert(`We are in business, ${data.email}`);
+      });
+    });
+  }
+
+
+  render() {
+    return (
+      <div className='settings-page'>
+
+        <StripeCheckout
+          token={this.onToken}
+          stripeKey={process.env.REACT_APP_STRIPE_TEST_KEY}
+          name='Cookbook Subscription'
+          zipcode={false}
+          amount={1000}
+          currency='USD'
+          email='cookbook_project@yahoo.com'
+        />
+      </div>
+    )
+  }
+}
