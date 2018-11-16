f9b7cce63ef3565add22b12279e1c14e09afb54b
Vu Cao <cdhvu@yahoo.com>
2018-11-15 16:42:34 -0800

    Updated scraper to whitelist geniuskitchen.com

--- cookbook/src/utils/scraper.js
+++ cookbook/src/utils/scraper.js
@@ -1,35 +1,69 @@
 import axios from "axios";
 
 const scraper = async url => {
-  try {
-    const response = await axios.get(
-      "https://allorigins.me/get?url=" + encodeURIComponent(url)
-    );
-    const html = response.data.contents;
-    const parser = new DOMParser();
-    const el = parser.parseFromString(html, "text/html");
-    const og_title = el.querySelector(`meta[property="og:title" ]`).content;
-    const og_sitename = el.querySelector(`meta[property="og:site_name" ]`)
-      .content;
-    const og_image = el.querySelector(`meta[property="og:image" ]`).content;
-    const og_desc = el.querySelector(`meta[name="description" ]`).content;
-    const prep_time = el.querySelector(`time[itemprop="prepTime"]`).textContent;
-    const cook_time = el.querySelector(`time[itemprop="cookTime"]`).textContent;
-    const total_time = el.querySelector(`time[itemprop="totalTime"]`)
-      .textContent;
-    const rating = el.querySelector(`meta[itemprop="ratingValue"]`).content;
-    return {
-      og_title,
-      og_sitename,
-      og_image,
-      og_desc,
-      prep_time,
-      cook_time,
-      total_time,
-      rating
-    };
-  } catch (error) {
-    console.log(error);
+  if (url.toLowerCase().includes("allrecipes")) {
+    try {
+      const response = await axios.get(
+        "https://allorigins.me/get?url=" + encodeURIComponent(url)
+      );
+      const html = response.data.contents;
+      const parser = new DOMParser();
+      const el = parser.parseFromString(html, "text/html");
+      const og_title = el.querySelector(`meta[property="og:title" ]`).content;
+      const og_sitename = "Allrecipes";
+      const og_image = el.querySelector(`meta[property="og:image" ]`).content;
+      const og_desc = el.querySelector(`meta[name="description" ]`).content;
+      const prep_time = el.querySelector(`span[class="ready-in-time"]`)
+        .textContent;
+      const rating = el.querySelector(`meta[itemprop="ratingValue"]`).content;
+      const servings = el.querySelector(`#metaRecipeServings`).content;
+      return {
+        og_title,
+        og_sitename,
+        og_image,
+        og_desc,
+        prep_time,
+        servings,
+        rating
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (url.toLowerCase().includes("geniuskitchen")) {
+    console.log(url);
+    try {
+      const response = await axios.get(
+        "https://allorigins.me/get?url=" + encodeURIComponent(url)
+      );
+      const html = response.data.contents;
+      const parser = new DOMParser();
+      const el = parser.parseFromString(html, "text/html");
+      // const og_icon = el.querySelector(`link[type="image/x-icon"].href`);
+      const og_title = el.querySelector(`meta[property="og:title"]`).content;
+      const og_sitename = "geniuskitchen.com";
+      const og_image = el.querySelector(`meta[property="og:image"]`).content;
+      const og_desc = el.querySelector(`meta[property="og:description" ]`)
+        .content;
+      const prep_time = el
+        .querySelector(`td.time`)
+        .textContent.replace(/[\n\r]+|[\s]{2,}/g, " ")
+        .trim()
+        .match(/\d+.+/g);
+      const servings = el.querySelector(`span.count`).textContent;
+      const rating = el.querySelector(`span[class="sr-only"]`).textContent;
+      return {
+        og_title,
+        og_sitename,
+        og_image,
+        og_desc,
+        prep_time,
+        servings,
+        rating
+      };
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
+
 export default scraper;
