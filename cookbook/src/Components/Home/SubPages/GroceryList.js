0f1e68f924517e6dfc203e508e576ef4fd0ea309
Katie Gorbell <katiegorbell@gmail.com>
2018-11-16 17:24:57 -0600

    switched calendar to react-day-picker for date range capabilities

--- cookbook/src/Components/Home/SubPages/GroceryList.js
+++ cookbook/src/Components/Home/SubPages/GroceryList.js
@@ -1,19 +1,85 @@
 import React, { Component } from "react";
-import Calendar from 'react-calendar';
+import Helmet from 'react-helmet';
+import DayPicker, { DateUtils } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
 
 class GroceryList extends Component {
-  constructor() {
-    super();
-    this.state = {
-    }
+  static defaultProps = {
+    numberOfMonths: 1,
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleDayClick = this.handleDayClick.bind(this);
+    this.handleResetClick = this.handleResetClick.bind(this);
+    this.state = this.getInitialState();
+  }
+
+  getInitialState() {
+    return {
+      from: undefined,
+      to: undefined,
+    };
+  }
+
+  handleDayClick(day) {
+    const range = DateUtils.addDayToRange(day, this.state);
+    this.setState(range);
+  }
+
+  handleResetClick() {
+    this.setState(this.getInitialState());
   }
 
 
   render() {
+    const { from, to } = this.state;
+    const modifiers = { start: from, end: to };
+
     return (
       <div className='grocery-list-page'>
         <div className='gen-list-container'>
-          <Calendar className='calendar'/>
+  <p>
+          {!from && !to && 'Please select the first day.'}
+          {from && !to && 'Please select the last day.'}
+          {from &&
+            to &&
+            `Selected from ${from.toLocaleDateString()} to
+                ${to.toLocaleDateString()}`}{' '}
+          {from &&
+            to && (
+              <button className="link" onClick={this.handleResetClick}>
+                Reset
+              </button>
+            )}
+        </p>
+        <DayPicker
+          className="Selectable"
+          numberOfMonths={this.props.numberOfMonths}
+          selectedDays={[from, { from, to }]}
+          modifiers={modifiers}
+          onDayClick={this.handleDayClick}
+        />
+        <Helmet>
+          <style>{`
+            .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
+              background-color: #f0f8ff !important;
+              color: #4a90e2;
+            }
+            .Selectable .DayPicker-Day {
+              border-radius: 0 !important;
+            }
+            .Selectable .DayPicker-Day--start {
+              border-top-left-radius: 50% !important;
+              border-bottom-left-radius: 50% !important;
+            }
+            .Selectable .DayPicker-Day--end {
+              border-top-right-radius: 50% !important;
+              border-bottom-right-radius: 50% !important;
+            }
+          `}</style>
+        </Helmet>
           <button>Generate List</button>
         </div>
         <div className='list'>
