bc359f093c8c466b8bf4b99b960ddd5ffba81e87
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-11-19 15:57:21 -0500

    resized images

--- cookbook/src/Components/Home/SubPages/GroceryList.js
+++ cookbook/src/Components/Home/SubPages/GroceryList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Helmet from 'react-helmet';
+// import Helmet from 'react-helmet';
 import DayPicker, { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
