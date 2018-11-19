56a18da3d41d53ea40a2d441f6c9126301107dec
Bradiowave <bradenw37@gmail.com>
2018-11-19 10:56:45 -0800

    create authLink for ApolloClient in index.js

--- cookbook/src/index.js
+++ cookbook/src/index.js
@@ -4,16 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Router } from 'react-router-dom';
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import history from './Auth/History.js';
 import auth from './Auth/Auth.js';
 
 const httpLink = new createHttpLink({
     uri: process.env.REACT_APP_CURR_ENV === "dev" ? 'http://localhost:4000' : process.env.REACT_APP_BACKEND_URL
-})
+});
+
+const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const id_token = localStorage.getItem('id_token');
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: id_token ? `Bearer ${id_token}` : "",
+        }
+    }
+});
   
 const client = new ApolloClient({
     link: httpLink,
@@ -26,7 +39,7 @@ const client = new ApolloClient({
             },
         }));
     },
-})
+});
 
 ReactDOM.render(
     <ApolloProvider client={client}>
