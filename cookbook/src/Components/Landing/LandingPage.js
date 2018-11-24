5780cfe50f88b3b4bae112579950323306cdadc6
Katie Gorbell <katiegorbell@gmail.com>
2018-11-23 18:52:15 -0600

    carousel added for recipe gallery onlanding page

--- cookbook/src/Components/Landing/LandingPage.js
+++ cookbook/src/Components/Landing/LandingPage.js
@@ -5,6 +5,7 @@ import auth from '../../Auth/Auth.js';
 import titleImg from '../../Images/titleImg.jpeg';
 import logo from '../../designs/Logo/CookBookLogo.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Slider from 'react-slick';
 
 
 class LandingPage extends Component {
@@ -14,6 +15,13 @@ class LandingPage extends Component {
 
     render() {
         const { isAuthenticated } = auth;
+        // Carousel settings:
+        var settings = {
+            infinite: true,
+            slidesToScroll: 4,
+            variableWidth: true,
+            centerMode: true
+          };
 
         return ( 
             <div className='containerLP'>
@@ -35,8 +43,31 @@ class LandingPage extends Component {
                 </div>
                 <div className="lp-content">
                     <div className='about-us'>
-                            <h1 className="about-header"><FontAwesomeIcon icon="utensils" className="fa"/>ABOUT US<FontAwesomeIcon icon="utensils" className="fa"/></h1>
-                            <p className='about-content'>Here at CookBook, we aim to be your one stop shop for meal planning. Through the use of our extensive gallery of recipes, our members are able to create a diverse schedule that fits their needs. our system will then generate your grocery list that can be easily accessed while on the go. Join for free today to see how we can get your meal-planning back on track.</p>
+                        <h1 className="about-header"><FontAwesomeIcon icon="utensils" className="fa"/>ABOUT US<FontAwesomeIcon icon="utensils" className="fa"/></h1>
+                        <p className='about-content'>Here at CookBook, we aim to be your one stop shop for meal planning. Through the use of our extensive gallery of recipes, our members are able to create a diverse schedule that fits their needs. our system will then generate your grocery list that can be easily accessed while on the go. Join for free today to see how we can get your meal-planning back on track.</p>
+                    </div>
+                    <div className="recipe-gallery">
+                        <h1 className="rg-header"><FontAwesomeIcon icon="utensils" className="fa"/>RECIPE GALLERY<FontAwesomeIcon icon="utensils" className="fa"/></h1>
+                        <Slider {...settings} className="carousel">
+                            <div>
+                                <img src={titleImg} alt='testig' className="carousel-img"/>
+                            </div>
+                            <div>
+                                <img src={titleImg} alt='testig' className="carousel-img"/>
+                            </div>
+                            <div>
+                                <img src={titleImg} alt='testig' className="carousel-img"/>
+                            </div>
+                            <div>
+                                <img src={titleImg} alt='testig' className="carousel-img"/>
+                            </div>
+                            <div>
+                                <img src={titleImg} alt='testig' className="carousel-img"/>
+                            </div>
+                            <div>
+                                <img src={titleImg} alt='testig' className="carousel-img"/>
+                            </div>
+                        </Slider>
                     </div>
                 </div>
             </div>
