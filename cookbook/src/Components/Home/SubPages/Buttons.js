5a42c7f41ec6b01c197621fbf772871dc579c550
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-11-27 17:14:43 -0500

    fixed errors

--- cookbook/src/Components/Home/SubPages/Buttons.js
+++ cookbook/src/Components/Home/SubPages/Buttons.js
@@ -7,6 +7,8 @@ class Buttons extends Component {
     this.isBreakfast = this.isBreakfast.bind(this);
     this.isLunch = this.isLunch.bind(this);
     this.isDinner = this.isDinner.bind(this);
+    this.isSnack = this.isSnack.bind(this);
+    this.isDessert = this.isDessert.bind(this);
     this.state = {
       type: ""
     }
@@ -48,23 +50,24 @@ class Buttons extends Component {
         <div className="recipe-buttons">
           <button 
             onClick={this.isBreakfast}>
-            {this.state.type === "breakfast" ? 'Breakfast Added' : 'Breakfast'}
+            {this.state.type === "breakfast" ? 'Selected' : 'Breakfast'}
           </button>
           <button
             onClick={this.isLunch}>
-            {this.state.type === "lunch" ? 'Lunch Added' : 'Lunch'}
+            {this.state.type === "lunch" ? 'Selected' : 'Lunch'}
           </button>
           <button
             onClick={this.isDinner}>
-            {this.state.type === "dinner" ? 'Dinner Added' : 'Dinner'}
+            {this.state.type === "dinner" ? 'Selected' : 'Dinner'}
           </button>
           <button 
             onClick={this.isSnack}>
-            {this.state.type === "snack" ? 'Snack Added' : 'Snack'}
+            {this.state.type === "snack" ? 'Selected' : 'Snack'}
           </button>
           <button 
-            onClick={this.isSnack}>
-            {this.state.type === "dessert" ? 'Dessert Added' : 'Dessert'}</button>
+            onClick={this.isDessert}>
+            {this.state.type === "dessert" ? 'Selected' : 'Dessert'}
+          </button>
           <button>Save</button>
         </div>
       </div>
