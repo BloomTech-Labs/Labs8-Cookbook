7ef0bbe70ae084f44aabeb8266c69712b4e6fe30
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-11-28 12:14:44 -0500

    fixed buttons

--- cookbook/src/Components/Home/SubPages/Buttons.js
+++ cookbook/src/Components/Home/SubPages/Buttons.js
@@ -2,41 +2,12 @@ import React, { Component } from "react";
 
 
 class Buttons extends Component {
-  constructor() {
-    super();
-    this.isBreakfast = this.isBreakfast.bind(this);
-    this.isLunch = this.isLunch.bind(this);
-    this.isDinner = this.isDinner.bind(this);
-    this.isSnack = this.isSnack.bind(this);
-    this.isDessert = this.isDessert.bind(this);
-    this.state = {
-      type: ""
-    }
-  }
 
-  isBreakfast = (e) =>  {
-    e.preventDefault(); 
-    this.setState({type: "breakfast"})
-  };
-
-  isLunch = (e) => {
+  mealButtonHandler = (e) =>  {
     e.preventDefault();
-    this.setState({type: "lunch"})
-  };
-
-  isDinner = (e) => {
-    e.preventDefault();
-    this.setState({type: "dinner"})
-  };
-
-  isSnack = (e) => {
-    e.preventDefault();
-    this.setState({type: "snack"})
-  };
-
-  isDessert = (e) => {
-    e.preventDefault();
-    this.setState({type: "dessert"})
+    this.setState({
+      type: e.target.name
+    })
   };
 
   render() {
@@ -44,24 +15,29 @@ class Buttons extends Component {
       <div className="recipe-inputs">
         <div className="recipe-buttons">
           <button 
-            onClick={this.isBreakfast}>
-            {this.state.type === "breakfast" ? 'Selected' : 'Breakfast'}
+            onClick={this.props.mealButtonHandler}
+            name="breakfast">
+            {this.props.type === "breakfast" ? 'Selected' : 'Breakfast'}
           </button>
           <button
-            onClick={this.isLunch}>
-            {this.state.type === "lunch" ? 'Selected' : 'Lunch'}
+            onClick={this.props.mealButtonHandler}
+            name="lunch">
+            {this.props.type === "lunch" ? 'Selected' : 'Lunch'}
           </button>
           <button
-            onClick={this.isDinner}>
-            {this.state.type === "dinner" ? 'Selected' : 'Dinner'}
+            onClick={this.props.mealButtonHandler}
+            name="dinner">
+            {this.props.type === "dinner" ? 'Selected' : 'Dinner'}
           </button>
           <button 
-            onClick={this.isSnack}>
-            {this.state.type === "snack" ? 'Selected' : 'Snack'}
+            onClick={this.props.mealButtonHandler}
+            name="snack">
+            {this.props.type === "snack" ? 'Selected' : 'Snack'}
           </button>
           <button 
-            onClick={this.isDessert}>
-            {this.state.type === "dessert" ? 'Selected' : 'Dessert'}
+            onClick={this.props.mealButtonHandler}
+            name="dessert">
+            {this.props.type === "dessert" ? 'Selected' : 'Dessert'}
           </button>
           <button>Save</button>
         </div>
