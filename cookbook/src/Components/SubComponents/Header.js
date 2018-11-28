3d6b76d0df21e818a4c037e6c0e5afe1f35c851b
Katie Gorbell <katiegorbell@gmail.com>
2018-11-28 17:47:48 -0600

    phone nav added, needs css

--- cookbook/src/Components/SubComponents/Header.js
+++ cookbook/src/Components/SubComponents/Header.js
@@ -103,6 +103,33 @@ class Header extends Component {
                         </div>
                     )}
                 </div>
+                <div className='phone-nav'>
+                    {renderIf(this.props.location.pathname !== '/home/create')(
+                        <div  className='create-nav'>
+                            <Link to='/home/create' style={{textDecoration: 'none', color: '#D85E50'}}>CREATE</Link>
+                        </div>
+                    )}
+                    {renderIf(this.props.location.pathname !== '/home/calendar')(
+                        <div className='calendar-nav'>
+                            <Link to='/home/calendar' style={{textDecoration: 'none', color: '#F5E6DC'}}>CALENDAR</Link>
+                        </div>
+                    )}
+                    {renderIf(this.props.location.pathname !== '/home/dashboard')(    
+                        <div className='dashboard-nav'>
+                            <Link to='/home/dashboard' style={{textDecoration: 'none', color: '#ffc988'}}>GROCERY LIST</Link>
+                        </div>
+                    )}
+                    {renderIf(this.props.location.pathname !== '/home/settings')(    
+                        <div className='settings-nav'>
+                            <Link to='/home/settings' style={{textDecoration: 'none', color: '#B3C1CC'}}>SETTINGS</Link>
+                        </div>
+                    )}
+                    {renderIf(!this.props.location.pathname.includes('/home/recipe'))(    
+                        <div className='recipes-nav'>
+                            <Link to='/home/recipes' style={{textDecoration: 'none', color: '#2E3650'}}>RECIPES</Link>
+                        </div>
+                    )}
+                </div>
             </div> 
         );
     }
