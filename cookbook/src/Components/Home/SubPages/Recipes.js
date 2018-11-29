3df092f111fc6622f260cda3c995188ffa9ec820
Katie Gorbell <katiegorbell@gmail.com>
2018-11-29 12:12:43 -0600

    query created for events

--- cookbook/src/Components/Home/SubPages/Recipes.js
+++ cookbook/src/Components/Home/SubPages/Recipes.js
@@ -8,31 +8,33 @@ const RECIPE_QUERY = gql`
     recipes {
       id
       title
-      readyInMinutes
-      servings
       image
+      events {
+        date
+        id
+      }
     }
   }
 `
 
-const dummyRecipesData = [
-  {
-    id: 0,
-    title: 'Deviled Eggs',
-    image: 'https://img.sndimg.com:443/food/image/upload/w_993,h_559,c_fill,fl_progressive,q_80/v1/img/recipes/48/15/3/TrKIAiXyStaqjpJUCaT0_food.com-28.jpg',
-    URL: 'https://www.geniuskitchen.com/recipe/deviled-eggs-48153',
-    meal: 'breakfast',
-    date: new Date( 'Wed Nov 21 2018 12:00:00 GMT-0800' )
-  },
-  {
-    id: 1,
-    title: 'Fettuccine Alfredo',
-    image: 'https://img.sndimg.com:443/food/image/upload/w_896,h_504,c_fill,fl_progressive,q_80/v1/img/recipes/85/96/7nqcwQZxSiOWr3jp4BgE_FettucineAlfredo3.jpg',
-    URL: 'https://www.geniuskitchen.com/recipe/olive-garden-fettuccine-alfredo-8596?ic1=suggestedAsset%7Cfettucine',
-    meal: 'dinner',
-    date: new Date( 'Thurs Nov 22 2018 12:00:00 GMT-0800' )
-  }
-]
+// const dummyRecipesData = [
+//   {
+//     id: 0,
+//     title: 'Deviled Eggs',
+//     image: 'https://img.sndimg.com:443/food/image/upload/w_993,h_559,c_fill,fl_progressive,q_80/v1/img/recipes/48/15/3/TrKIAiXyStaqjpJUCaT0_food.com-28.jpg',
+//     URL: 'https://www.geniuskitchen.com/recipe/deviled-eggs-48153',
+//     meal: 'breakfast',
+//     date: new Date( 'Wed Nov 21 2018 12:00:00 GMT-0800' )
+//   },
+//   {
+//     id: 1,
+//     title: 'Fettuccine Alfredo',
+//     image: 'https://img.sndimg.com:443/food/image/upload/w_896,h_504,c_fill,fl_progressive,q_80/v1/img/recipes/85/96/7nqcwQZxSiOWr3jp4BgE_FettucineAlfredo3.jpg',
+//     URL: 'https://www.geniuskitchen.com/recipe/olive-garden-fettuccine-alfredo-8596?ic1=suggestedAsset%7Cfettucine',
+//     meal: 'dinner',
+//     date: new Date( 'Thurs Nov 22 2018 12:00:00 GMT-0800' )
+//   }
+// ]
 
 class Recipes extends Component {
   constructor(props) {
@@ -91,11 +93,10 @@ class Recipes extends Component {
         
         <Query query={RECIPE_QUERY}>
           {({ loading, error, data }) => {
-            // if (loading) return <div>Fetching</div>
-            // if (error) return <div>Error</div>
+            if (loading) return <div>Fetching</div>
+            if (error) return <div>Error</div>
             
-            // const recipesToRender = data.recipes
-            const recipesToRender = dummyRecipesData
+            const recipesToRender = data.recipes
 
             return (
               <div className="recipesCards">
