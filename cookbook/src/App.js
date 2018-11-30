4dcb5490cc15a661ab9c97812a73c8c3a650e529
Katie Gorbell <katiegorbell@gmail.com>
2018-11-29 22:10:29 -0600

    styling added for recipe card on recipes page

--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -10,10 +10,10 @@ import Callback from "./Auth/Callback.js";
 import { library } from "@fortawesome/fontawesome-svg-core";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // Font Awesome Icons:
-import { faUtensils, faLink } from "@fortawesome/free-solid-svg-icons";
+import { faUtensils, faLink, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import Signup from "./Components/SubComponents/Signup";
 
-library.add(faUtensils, faLink);
+library.add(faUtensils, faLink, faTrashAlt);
 
 class App extends Component {
   state = {
--- cookbook/src/Components/Home/SubPages/RecipeCard.js
+++ cookbook/src/Components/Home/SubPages/RecipeCard.js
@@ -1,59 +1,54 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 class RecipeCard extends Component {
 
-  handleLink() {
-    window.open(this.props.recipe.URL, '_blank');
-  }
-
-  handleDelete() {
-    console.log(`Delete note ${this.props.recipe.id}`)
-  }
-
   isSearchedFor() {
     // searchInContent will only be true if the search term is in the title, meal, or date of the RecipeCard
     let searchInContent = (~this.props.recipe.title.toLowerCase().indexOf(this.props.search.toLowerCase())) ||
-                          (~this.props.recipe.meal.toLowerCase().indexOf(this.props.search.toLowerCase())) ||
-                          (~this.props.recipe.date.toLocaleDateString().toLowerCase().indexOf(this.props.search.toLowerCase()));
+                          (~this.props.recipe.event.mealType.toLowerCase().indexOf(this.props.search.toLowerCase())) ||
+                          (~this.props.recipe.event.date.toLocaleDateString().toLowerCase().indexOf(this.props.search.toLowerCase()));
 
     let isInFilter = this.props.filter.size === 0 || this.props.filter.has(this.props.recipe.meal);
 
     return searchInContent && isInFilter;
   }
 
+
   render() {
     // URL for each individual recipe to be passed in to route
     const recipeUrl = this.props.recipe.title.split(' ').join('-');
-
     return (
       // make the RecipeCard visible only if searched for
       <div className={this.isSearchedFor() ? "recipeCard" : "recipeCardInvisible"}>
         <div className='content-container'>
-          
           <div className="title">
             {this.props.recipe.title}
           </div>
-          
           <Link to={{pathname: `/home/recipe/${recipeUrl}`, state: this.props.recipe}}>
-            <div className='link' style={{backgroundImage: `url(${this.props.recipe.image})`}}></div>
+            <div className='image' style={{backgroundImage: `url(${this.props.recipe.image})`}}></div>
           </Link>
-            
-          <button onClick={() => this.handleLink()} className="web-link">Link</button>
-          
-          <div className="schedule">
-            <span>scheduled for</span>
-            {this.props.recipe.events.map(event => 
-              <div className='event'>
-                <div className="meal">{event.mealType}</div>
-                <div className="date">{event.date}</div>
+          <div className='card-bottom'>  
+              <a href={'' + this.props.recipe.url} className='link-container'>
+                <FontAwesomeIcon icon='link' className='web-link'/>
+                <span className='link-text'>link</span>
+              </a>
+              <div className="schedule">
+                <span>scheduled for</span>
+                {this.props.recipe.events.map(event => 
+                  <div className='event'>
+                    <div>{event.mealType}</div>
+                    <div>{event.date}</div>
+                  </div>
+                )}
               </div>
-            )}
+            <button className="del-button">
+              <FontAwesomeIcon icon='trash-alt' className='del-icon'/>
+              <span className='del-text'>Delete</span>
+            </button>
           </div>
-          
-          <button onClick={() => this.handleDelete()} className="del-button">Delete</button>
-
         </div>
       </div>
     )
