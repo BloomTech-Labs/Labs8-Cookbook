cdc2448ede680ca4eb198e5023654ed1f083cc03
Vu Cao <cdhvu@yahoo.com>
2018-12-02 22:48:03 -0800

    Create helper functions
    getUserID to get current user id.
    getRecipe to check if a specific recipe already exists for current user.

---
+++ Backend/src/utils/helper.js
@@ -0,0 +1,25 @@
+const getUserId = async ctx => {
+  const authUser = await ctx.user;
+  const auth0sub = authUser.sub;
+  const user = await ctx.db.query.user({
+    where: {
+      auth0Sub: auth0sub
+    }
+  });
+
+  return user.id;
+};
+
+const getRecipe = async (ctx, title, userId) => {
+  const recipes = await ctx.db.query.recipes({
+    where: {
+      title: title,
+      createdBy: {
+        id: userId
+      }
+    }
+  });
+  return recipes.length ? recipes[0] : null;
+};
+
+module.exports = { getUserId, getRecipe };
