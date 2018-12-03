5eb9aaaff29b6088954fec4c3643318d6fc810bf
Bradiowave <bradenw37@gmail.com>
2018-12-03 12:31:34 -0800

    make links in footer redirect

--- cookbook/src/Components/SubComponents/Footer.js
+++ cookbook/src/Components/SubComponents/Footer.js
@@ -1,22 +1,49 @@
 import React from 'react';
 import Logo from '../../designs/Logo/CookBookLogo.svg';
+import { withRouter } from "react-router-dom";
 
+const Footer = (props) => {
+    const handleRedirect = (path) => {
+        props.history.push(path);
+    }
 
-const Footer = () => {
     return ( 
         <div className='footer'>
-            <div className='content'>
-                <img className='logo' src={Logo} alt='company logo'/>
-                <div className='created-by'>
-                    <h3>Created By:</h3>
-                    <span>Vu Cao</span>
-                    <span>Katie Gorbell</span>
-                    <span>Arthur Pisakhov</span>
-                    <span>Braden Walker</span>
-                </div>
+            
+            <img className='logo' src={Logo} alt='company logo'/>
+            
+            <div className='title'>COOKBOOK</div>
+            
+            <div className='nav'>
+                <p onClick={() => handleRedirect('/home/create')}>CREATE</p>
+                <p onClick={() => handleRedirect('/home/recipes')}>RECIPES</p>
+                <p onClick={() => handleRedirect('/home/calendar')}>CALENDAR</p>
+                <p onClick={() => handleRedirect('/home/dashboard')}>GROCERY LIST</p>
+                <p onClick={() => handleRedirect('/home/settings')}>SETTINGS</p>
             </div>
+            
+            <div className="info">
+
+                <div className="contact-container">
+                    <p className="label">contact</p>
+                    <p>cookbook_project@yahoo.com</p>
+                </div>
+
+                <div className="team-container">
+                    <p className="label">team</p>
+
+                    <div className='names'>
+                        <p>vu cao</p>
+                        <p>katie gorbell</p>
+                        <p>arthur pisakhov</p>
+                        <p>braden walker</p>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     );
 }
  
-export default Footer;
+export default withRouter(Footer);
