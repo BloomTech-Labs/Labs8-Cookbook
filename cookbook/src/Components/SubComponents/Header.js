8d25f4c55529161bd2971fcd5381108b65241f68
Bradiowave <bradenw37@gmail.com>
2018-12-03 23:24:07 -0800

    basic nav phone styling

--- cookbook/src/Components/SubComponents/Header.js
+++ cookbook/src/Components/SubComponents/Header.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import Logo from '../../designs/Logo/CookBookLogo.svg';
-import { Link } from 'react-router-dom';
 import auth from '../../Auth/Auth.js';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import renderIf from 'render-if';
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showPhoneNav: false,
+        };
+    }
+
     static propTypes = {
         location: PropTypes.object.isRequired,
     }
 
     logout() {
         auth.logout();
+    }
+
+    handlePhoneNavClass = (path) => {
+        if (this.props.location.pathname.includes(path)) {
+            return `${path}-phone-nav-active`
+        }
+        if (this.state.showPhoneNav) {
+            return `${path}-phone-nav-show`
+        }
+        return 'phone-nav-hide'
+    }
+
+    handleRedirect = (path) => {
+        this.props.history.push(path);
+    }
+
+    togglePhoneNav = () => {
+        this.setState({showPhoneNav: !this.state.showPhoneNav})
     }
 
     render() {
@@ -24,7 +47,7 @@ class Header extends Component {
                 <div className='header-container'>
                     <div className='header'>
 
-                        <img className='logo' src={Logo} alt='COOKBOOK logo'/>
+                        <img className='logo' src={Logo} alt='logo'/>
 
                         <p className="title">COOKBOOK</p>
 
@@ -32,9 +55,19 @@ class Header extends Component {
 
                     </div>
                 </div>
-
                 
                 <div className='nav'>
+
+                    <div className='phone-nav'>
+                        <div onClick={() => this.togglePhoneNav()} className='toggle-phone-nav'>...</div>
+                        <div className='links'>
+                            <p onClick={() => this.handleRedirect('/home/create')} className={this.handlePhoneNavClass('create')}>CREATE</p>
+                            <p onClick={() => this.handleRedirect('/home/recipes')} className={this.handlePhoneNavClass('recipes')}>RECIPES</p>
+                            <p onClick={() => this.handleRedirect('/home/calendar')} className={this.handlePhoneNavClass('calendar')}>CALENDAR</p>
+                            <p onClick={() => this.handleRedirect('/home/dashboard')} className={this.handlePhoneNavClass('dashboard')}>GROCERY LIST</p>
+                            <p onClick={() => this.handleRedirect('/home/settings')} className={this.handlePhoneNavClass('settings')}>SETTINGS</p>
+                        </div>
+                    </div>
                     
                 </div>
 
