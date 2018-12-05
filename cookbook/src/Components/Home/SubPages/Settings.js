04316f7196593f62a3b380e2b268d5a48a1d46ed
Vu Cao <cdhvu@yahoo.com>
2018-12-04 21:15:35 -0800

    Update Stripe component in Settings

--- cookbook/src/Components/Home/SubPages/Settings.js
+++ cookbook/src/Components/Home/SubPages/Settings.js
@@ -1,50 +1,54 @@
-
-import React from 'react'
-import StripeCheckout from 'react-stripe-checkout';
-import { Mutation } from 'react-apollo';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 
-  const CREATE_SUBSCRIPTION_MUTATION = gql`
-    mutation createSubscription($token: String!) {
-      createSubscription(token: $token) {
+const CREATE_SUBSCRIPTION_MUTATION = gql`
+  mutation createSubscription($token: String!) {
+    createSubscription(token: $token) {
+      id
+      amount
+      charge
+      user {
         id
-        type
-        amount
-        # userid
-        # charge
+        email
       }
-    } 
-  `
+    }
+  }
+`;
 
 export default class Settings extends React.Component {
-  onToken = (res, createSubscription) => {
-    console.log(res.id);
-    createSubscription({
-      variables: {
-        token: res.id
-      }
-    }).catch(error =>{
-      console.log(error.message);
-    })
-  }
+  onToken = async (res, createSubscription) => {
+    try {
+      console.log(res);
+      await createSubscription({
+        variables: {
+          token: res.id
+        }
+      });
+    } catch (error) {
+      console.log("FE Stripe error: ", error.message);
+      return error.message;
+    }
+  };
 
   render() {
     return (
-      <div className='settings-page'>
+      <div className="settings-page">
         <Mutation mutation={CREATE_SUBSCRIPTION_MUTATION}>
-        {createSubscription => (
-          <StripeCheckout
-            stripeKey='pk_test_FyA4hajfxfEQ4jCcEaeQtTIL'
-            name='Cookbook Subscription'
-            zipcode={false}
-            amount={1000}
-            currency='USD'
-            email='cookbook_project@yahoo.com'
-            token = {res => this.onToken(res, createSubscription)}
-          />
-        )}
+          {createSubscription => (
+            <StripeCheckout
+              stripeKey="pk_test_FyA4hajfxfEQ4jCcEaeQtTIL"
+              name="Cookbook Subscription"
+              zipcode={false}
+              amount={1000}
+              currency="USD"
+              email="cookbook_project@yahoo.com"
+              token={res => this.onToken(res, createSubscription)}
+            />
+          )}
         </Mutation>
       </div>
-    )
+    );
   }
 }
