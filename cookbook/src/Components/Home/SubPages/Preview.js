47e6d1678cfec33362e09395af264cca62fd6274
Bradiowave <bradenw37@gmail.com>
2018-12-04 23:33:42 -0800

    fix image whitespace on bottom

--- cookbook/src/Components/Home/SubPages/Preview.js
+++ cookbook/src/Components/Home/SubPages/Preview.js
@@ -6,9 +6,7 @@ const Preview = props => {
   const preview = props.og_title ? (
     <div className='preview'>
       <div className='title'>{props.og_title}</div>
-      <div className='image-container'>
-        <img className='image' src={props.og_image} alt={props.title} />
-      </div>
+      <img className='image' src={props.og_image} alt={props.title} />
       <div className='body'>
         <div className='description'>{props.og_desc}</div>
         <div className='site'>{props.og_sitename}</div>
