f3720063ba8d11fd0782c10a96a74d8fa91c20e4
Vu Cao <cdhvu@yahoo.com>
2018-12-04 22:40:07 -0800

    Fix StripeCheckout component

--- cookbook/src/Components/Home/SubPages/Settings.js
+++ cookbook/src/Components/Home/SubPages/Settings.js
@@ -1,7 +1,8 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
-import { Mutation } from "react-apollo";
+import { graphql, compose } from "react-apollo";
 import gql from "graphql-tag";
+import { CURRENT_USER_QUERY } from "../SubPages/User";
 
 const CREATE_SUBSCRIPTION_MUTATION = gql`
   mutation createSubscription($token: String!) {
@@ -17,7 +18,7 @@ const CREATE_SUBSCRIPTION_MUTATION = gql`
   }
 `;
 
-export default class Settings extends React.Component {
+class Settings extends React.Component {
   onToken = async (res, createSubscription) => {
     try {
       console.log(res);
@@ -33,22 +34,43 @@ export default class Settings extends React.Component {
   };
 
   render() {
+    if (this.props.userData.loading) {
+      return <div>Loading...</div>;
+    }
     return (
       <div className="settings-page">
-        <Mutation mutation={CREATE_SUBSCRIPTION_MUTATION}>
-          {createSubscription => (
-            <StripeCheckout
-              stripeKey="pk_test_FyA4hajfxfEQ4jCcEaeQtTIL"
-              name="Cookbook Subscription"
-              zipcode={false}
-              amount={1000}
-              currency="USD"
-              email="cookbook_project@yahoo.com"
-              token={res => this.onToken(res, createSubscription)}
-            />
-          )}
-        </Mutation>
+        <div className="user-info">
+          <h3>First Name:</h3>
+          <span>{this.props.userData.currentUser.firstName}</span>
+          <h3>Last Name:</h3>
+          <span>{this.props.userData.currentUser.lastName}</span>
+          <h3>Email:</h3>
+          <span>{this.props.userData.currentUser.email}</span>
+        </div>
+        <StripeCheckout
+          stripeKey="pk_test_FyA4hajfxfEQ4jCcEaeQtTIL"
+          name="Cookbook Subscription"
+          zipcode={false}
+          amount={1000}
+          currency="USD"
+          email={this.props.userData.currentUser.email}
+          token={res => this.onToken(res, this.props.createSubscription)}
+        >
+          <button className="stripe-btn">
+            <span>Subscribe</span>
+          </button>
+        </StripeCheckout>
       </div>
     );
   }
 }
+
+const getUserQuery = graphql(CURRENT_USER_QUERY, { name: "userData" });
+const createSubscriptionMutation = graphql(CREATE_SUBSCRIPTION_MUTATION, {
+  name: "createSubscription"
+});
+
+export default compose(
+  getUserQuery,
+  createSubscriptionMutation
+)(Settings);
