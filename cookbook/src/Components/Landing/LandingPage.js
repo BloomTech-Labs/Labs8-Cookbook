19b038c4808c3f0d3ebf5d149b4ddfc87d8cfda5
Bradiowave <bradenw37@gmail.com>
2018-12-11 11:06:00 -0800

    branding complete

--- cookbook/src/Components/Landing/LandingPage.js
+++ cookbook/src/Components/Landing/LandingPage.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import auth from '../../Auth/Auth.js';
 
 import titleImg from '../../Images/titleImg.jpeg';
-import logo from '../../designs/Logo/CookBookLogo.svg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import logo from '../../designs/Logo/CookBookLogoWithShadow.svg';
 
 const { isAuthenticated } = auth;
 
@@ -17,35 +16,51 @@ class LandingPage extends Component {
     }
 
     render() {
-        const { isAuthenticated } = auth;
 
-        return ( 
-            <div className='containerLP'>
-                <div className='titleContainer'>
-                    <img className="bckgImg" src={titleImg} alt="table with produce on top." />
-                    <div className='landingNav'>
-                        {
-                            !isAuthenticated() && (
-                            <div className="register" onClick={this.login.bind(this)}>
-                                Login/Register
-                            </div>
-                            )
-                        }
-                    </div>
-                    <div className='logo-title'>
+        return (
+            <div className='landing-container'>
+                <div className='above-the-fold' style={{backgroundImage: `url(${titleImg})`}}>
+
+                    <div className='branding'>
+
                         <img className="logo" src={logo} alt='company logo'/>
-                        <span className='title'>COOKBOOK</span>
+
+                        <div className='title'>
+
+                            <div className='name'>
+                                <span>C</span><span>O</span><span>O</span><span>K</span>
+                                <span>B</span><span>O</span><span>O</span><span>K</span>
+                            </div>
+
+                            <p className='hook'>All of your recipes. All in one place.</p>
+
+                        </div>
+
                     </div>
-                </div>
-                <div className="lp-content">
-                    <div className='about-us'>
-                        <h1 className="about-header"><FontAwesomeIcon icon="utensils" className="fa"/>ABOUT US<FontAwesomeIcon icon="utensils" className="fa"/></h1>
-                        <p className='about-content'>Here at CookBook, we aim to be your one stop shop for meal planning. Through the use of our extensive gallery of recipes, our members are able to create a diverse schedule that fits their needs. our system will then generate your grocery list that can be easily accessed while on the go. Join for free today to see how we can get your meal-planning back on track.</p>
-                    </div>
+
+                    <div className='call-to-action' onClick={this.login.bind(this)}>login / register</div>
+
                 </div>
             </div>
+        )
+
+        // return (
+        //     <div className='containerLP'>
+        //         <div className='titleContainer'>
+        //             <img className="bckgImg" src={titleImg} alt="table with produce on top." />
+        //             <div className='landingNav'>
+        //                 <div className="register" onClick={this.login.bind(this)}>
+        //                     Login/Register
+        //                 </div>
+        //             </div>
+        //             <div className='logo-title'>
+        //                 <img className="logo" src={logo} alt='company logo'/>
+        //                 <span className='title'>COOKBOOK</span>
+        //             </div>
+        //         </div>
+        //     </div>
     
-         );
+        // );
     }
 }
  
