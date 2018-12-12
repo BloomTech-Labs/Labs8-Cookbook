83cfd028ac8fe9be67663d52221239cd205e6d5b
Vu Cao <cdhvu@yahoo.com>
2018-12-11 23:09:01 -0800

    Minor bug fix

--- cookbook/src/utils/toastify.js
+++ cookbook/src/utils/toastify.js
@@ -4,20 +4,8 @@ function toastMessage(type, message) {
   if (type === "error") {
     return toast.error(message, { className: "toast-error" });
   } else if (type === "success") {
-    return toast.error(message, { className: "toast-success" });
+    return toast.success(message, { className: "toast-success" });
   }
 }
 
-function toastLoading(message) {
-  return toast(message, {
-    type: toast.TYPE.INFO,
-    className: "toast-loading",
-    autoClose: false
-  });
-}
-
-function toastUpdate(id, message) {
-  return toast.update(id, { render: message, autoClose: 3000 });
-}
-
-export { toastLoading, toastUpdate, toastMessage };
+export { toastMessage };
