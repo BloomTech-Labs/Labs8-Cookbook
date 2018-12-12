f4514c6b439a204a7720084a9d23f5f26235ba61
Vu Cao <cdhvu@yahoo.com>
2018-12-11 22:26:37 -0800

    Change logo in Header to redirect to recipes page

--- cookbook/src/Components/SubComponents/Header.js
+++ cookbook/src/Components/SubComponents/Header.js
@@ -1,84 +1,86 @@
-import React, { Component } from 'react';
-import Logo from '../../designs/Logo/CookBookLogo.svg';
-import { Link } from 'react-router-dom';
-import auth from '../../Auth/Auth.js';
-import { withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
-
+import React, { Component } from "react";
+import Logo from "../../designs/Logo/CookBookLogo.svg";
+import { Link } from "react-router-dom";
+import auth from "../../Auth/Auth.js";
+import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
 
 class Header extends Component {
-    static propTypes = {
-        location: PropTypes.object.isRequired,
-    }
+  static propTypes = {
+    location: PropTypes.object.isRequired
+  };
 
-    logout() {
-        auth.logout();
-    }
+  logout() {
+    auth.logout();
+  }
 
-    //change routes on nav click
-    handleRedirect = (path) => {
-        this.props.history.push(path);
-    }
+  //change routes on nav click
+  handleRedirect = path => {
+    this.props.history.push(path);
+  };
 
-    //change routes on nav click
-    //if the user clicks on the current page it will toggle the nav bar
-    handlePhoneRedirect = (path) => {
-        if (this.props.location.pathname.includes(path)) this.togglePhoneNav();
-        else this.props.history.push(path);
-    }
+  //change routes on nav click
+  //if the user clicks on the current page it will toggle the nav bar
+  handlePhoneRedirect = path => {
+    if (this.props.location.pathname.includes(path)) this.togglePhoneNav();
+    else this.props.history.push(path);
+  };
 
-    //determine whether a nav item is active, shown, or hidden
-    handlePhoneNavClass = (path) => {
-        if (this.props.location.pathname.includes(path)) {
-            return `${path}-phone-nav-active`
-        }
-        if (this.state.showPhoneNav) {
-            return `${path}-phone-nav-show`
-        }
-        return 'phone-nav-hide'
+  //determine whether a nav item is active, shown, or hidden
+  handlePhoneNavClass = path => {
+    if (this.props.location.pathname.includes(path)) {
+      return `${path}-phone-nav-active`;
     }
-
-    //toggle the expanded phone nav bar
-    togglePhoneNav = () => {
-        this.setState({showPhoneNav: !this.state.showPhoneNav})
+    if (this.state.showPhoneNav) {
+      return `${path}-phone-nav-show`;
     }
+    return "phone-nav-hide";
+  };
 
-    //determine the className of the phone toggle button for appropriate color change
-    handleTogglePhoneNavClass = () => {
-        if (this.props.location.pathname.includes('create')) return 'create-toggle-phone-nav';
-        if (this.props.location.pathname.includes('recipe')) return 'recipe-toggle-phone-nav';
-        if (this.props.location.pathname.includes('calendar')) return 'calendar-toggle-phone-nav';
-        if (this.props.location.pathname.includes('dashboard')) return 'dashboard-toggle-phone-nav';
-        if (this.props.location.pathname.includes('settings')) return 'settings-toggle-phone-nav';
+  //toggle the expanded phone nav bar
+  togglePhoneNav = () => {
+    this.setState({ showPhoneNav: !this.state.showPhoneNav });
+  };
+
+  //determine the className of the phone toggle button for appropriate color change
+  handleTogglePhoneNavClass = () => {
+    if (this.props.location.pathname.includes("create"))
+      return "create-toggle-phone-nav";
+    if (this.props.location.pathname.includes("recipe"))
+      return "recipe-toggle-phone-nav";
+    if (this.props.location.pathname.includes("calendar"))
+      return "calendar-toggle-phone-nav";
+    if (this.props.location.pathname.includes("dashboard"))
+      return "dashboard-toggle-phone-nav";
+    if (this.props.location.pathname.includes("settings"))
+      return "settings-toggle-phone-nav";
+  };
+
+  //determine if a navbar item should go across the entire screen or not
+  handleNormalNavClass = path => {
+    if (this.props.location.pathname.includes(path)) {
+      return `${path}-normal-nav-active`;
     }
+    return `${path}-normal-nav-inactive`;
+  };
 
-    //determine if a navbar item should go across the entire screen or not
-    handleNormalNavClass = (path) => {
-        if (this.props.location.pathname.includes(path)) {
-            return `${path}-normal-nav-active`
-        }
-        return `${path}-normal-nav-inactive`
-    }
+  render() {
+    const { isAuthenticated } = auth;
 
-    render() {
-        const { isAuthenticated } = auth;
-
-        return (
-            <div className='header'>
-                <Link className='link' to='/home'>
-                    <img className='logo' src={Logo} alt='COOKBOOK logo'/>
-                </Link>
-                <span className="title">COOKBOOK</span>
-                {
-                    isAuthenticated() && (
-                    <div className="signout" onClick={this.logout.bind(this)}>
-                        logout
-                    </div>
-                    )
-                }
-            </div>
-        );
-    }
+    return (
+      <div className="header">
+        <Link className="link" to="/home/recipes">
+          <img className="logo" src={Logo} alt="COOKBOOK logo" />
+        </Link>
+        <span className="title">COOKBOOK</span>
+        {isAuthenticated() && (
+          <div className="signout" onClick={this.logout.bind(this)}>
+            logout
+          </div>
+        )}
+      </div>
+    );
+  }
 }
- 
+
 export default withRouter(Header);
