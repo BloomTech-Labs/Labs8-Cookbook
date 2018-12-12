70eda7e419c1adb924c291d1b91f27f2b49972b7
Bradiowave <bradenw37@gmail.com>
2018-12-12 09:27:50 -0800

    add image and title onto recipe cards

--- cookbook/src/Components/Home/SubPages/RecipeCard.js
+++ cookbook/src/Components/Home/SubPages/RecipeCard.js
@@ -37,51 +37,93 @@ class RecipeCard extends Component {
   render() {
     // URL for each individual recipe to be passed in to route
     const recipeUrl = this.props.recipe.title.split(" ").join("-");
+
     return (
-      // make the RecipeCard visible only if searched for
-      <div className="recipeCard">
-        <div className="content-container">
-          <div className="title">{this.props.recipe.title}</div>
-          <Link
-            to={{
-              pathname: `/home/recipe/${recipeUrl}`,
-              state: this.props.recipe
-            }}
-          >
-            <div
-              className="image"
-              style={{ backgroundImage: `url(${this.props.recipe.image})` }}
-            />
-          </Link>
-          <div className="card-bottom">
-            <a
-              href={"" + this.props.recipe.url}
-              rel="noopener noreferrer"
-              target="_blank"
-              className="link-container"
-            >
-              <FontAwesomeIcon icon="link" className="web-link" />
-              <span className="link-text">link</span>
-            </a>
-            <div className="schedule">
+      <div className='recipe-card-container'>
+
+        <div className='image-and-bottom'>
+
+          <div className='image-container' style={{backgroundImage: `url(${this.props.recipe.image})`}}></div>
+
+          <div className='card-bottom'>
+
+            <div className='title-container'>
+              <span className='title'>{this.props.recipe.title}</span>
+            </div>
+
+            <div className="schedule-container">
               <CardSchedule
                 key={this.props.recipe.events.length}
                 events={this.props.recipe.events}
                 recipeId={this.props.recipe.id}
               />
             </div>
-            <button className="del-button">
-              <FontAwesomeIcon
-                icon="trash-alt"
-                className="del-icon"
-                onClick={this.deleteHandler}
-              />
-              <span className="del-text">Delete</span>
-            </button>
+
           </div>
+        
         </div>
+
       </div>
     );
+
+    // return (
+    //   <div className="recipeCard">
+
+    //     <div className="content-container">
+
+    //       <div className="title">{this.props.recipe.title}</div>
+
+    //       <Link
+    //         to={{
+    //           pathname: `/home/recipe/${recipeUrl}`,
+    //           state: this.props.recipe
+    //         }}
+    //       >
+    //         <div
+    //           className="image"
+    //           style={{ backgroundImage: `url(${this.props.recipe.image})` }}
+    //         />
+    //       </Link>
+
+    //       <div className="card-bottom">
+
+    //         <a
+    //           href={"" + this.props.recipe.url}
+    //           rel="noopener noreferrer"
+    //           target="_blank"
+    //           className="link-container"
+    //         >
+    //           <FontAwesomeIcon icon="link" className="web-link" />
+
+    //           <span className="link-text">link</span>
+    //         </a>
+
+    //         <div className="schedule">
+    //           <CardSchedule
+    //             key={this.props.recipe.events.length}
+    //             events={this.props.recipe.events}
+    //             recipeId={this.props.recipe.id}
+    //           />
+    //         </div>
+
+    //         <button className="del-button">
+
+    //           <FontAwesomeIcon
+    //             icon="trash-alt"
+    //             className="del-icon"
+    //             onClick={this.deleteHandler}
+    //           />
+
+    //           <span className="del-text">Delete</span>
+
+    //         </button>
+
+    //       </div>
+
+    //     </div>
+
+    //   </div>
+    // );
   }
 }
 
