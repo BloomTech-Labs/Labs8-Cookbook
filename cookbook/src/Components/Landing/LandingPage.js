ac8d46bc2731fe0097b2179687591a29f47008a5
Vu Cao <cdhvu@yahoo.com>
2018-12-13 16:12:05 -0800

    Remove console.log

--- cookbook/src/Components/Landing/LandingPage.js
+++ cookbook/src/Components/Landing/LandingPage.js
@@ -98,24 +98,6 @@ class LandingPage extends Component {
         </div>
       </div>
     );
-
-    // return (
-    //     <div className='containerLP'>
-    //         <div className='titleContainer'>
-    //             <img className="bckgImg" src={titleImg} alt="table with produce on top." />
-    //             <div className='landingNav'>
-    //                 <div className="register" onClick={this.login.bind(this)}>
-    //                     Login/Register
-    //                 </div>
-    //             </div>
-    //             <div className='logo-title'>
-    //                 <img className="logo" src={logo} alt='company logo'/>
-    //                 <span className='title'>COOKBOOK</span>
-    //             </div>
-    //         </div>
-    //     </div>
-
-    // );
   }
 }
 
